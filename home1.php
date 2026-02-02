999360376cc3287187b56c801d1cd0f3ca8e84fb
dev-nillo <dmdcribeiro@gmail.com>
2026-02-01 21:40:35 -0300

    Add files via upload

---
+++ home1.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página inicial - Loja virtual</title>
+    <link href="home1.css" rel="stylesheet">
+     <link href='' rel='stylesheet'>
+    <script defer src="home1.js"></script>
+</head>
+<body>
+    <header class="header">
+
+            <nav class="nav">
+                 <a href="home1.php"><img src="imagens e recursos p. site/logo.png" width="70px"
+        height="70px"
+        class="logo"></a>  
+        <button class="hamburguer"></button>
+        <ul class="nav-list">
+            <li><i class="bx bxs-user"></i><a href="login.php" class="navbar">Login</a></li>
+            <li><a href="contato.html" class="navbar">Contato</a></li>
+            <li><a href="https://wa.me/message/4DBTGSHFC35ND1" class="navbar">Contato do DEV</a></li>
+        </ul>
+    </nav>
+    </header>
+<main>    
+    <section class="apresentacao">
+        <p>Bem-vindo à Kylinda Perfumaria!</p>
+        Sua loja virtual de perfumes e cosméticos.
+        <h4>*está é apenas uma página de apresentação da Loja*</h4>
+        <h4 id="navegue">Navegue com o menu acima</h4>
+    </section>
+</main>
+    <footer>
+        <p><a href="sistema.php">&copy;</a> 2026 Loja Virtual Kylinda Perfumaria. Todos os direitos reservados.</p>
+    </footer>
+    </body>
+</html>
